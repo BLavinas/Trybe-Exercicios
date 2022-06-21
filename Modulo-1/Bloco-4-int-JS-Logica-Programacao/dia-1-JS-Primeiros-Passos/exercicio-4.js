dcc88b717387d4b70f6c01dc6a77fbad1d525d5a
Bernardo Lavinas <bernardorclavinas@gmail.com>
2022-06-21 18:54:25 -0300

    Exercicio 4 Bloco 4 - Introdução à JavaScript e logica de programação realizado

---
+++ Modulo-1/Bloco-4-int-JS-Logica-Programacao/dia-1-JS-Primeiros-Passos/exercicio-4.js
@@ -0,0 +1,10 @@
+const positiveNumber = 5;
+if (positiveNumber > 0){
+    console.log("positive");
+}
+else if (positiveNumber < 0) {
+    console.log("negative");
+}
+else {
+    console.log("zero");
+}
