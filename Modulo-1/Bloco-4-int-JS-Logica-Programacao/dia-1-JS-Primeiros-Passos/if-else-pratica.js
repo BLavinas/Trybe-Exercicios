c07072970b562afb4d3e213b9cd7b1164c73d24a
Bernardo Lavinas <bernardorclavinas@gmail.com>
2022-06-21 14:40:53 -0300

    Exercicio para praticar If/else realizado

---
+++ Modulo-1/Bloco-4-int-JS-Logica-Programacao/dia-1-JS-Primeiros-Passos/if-else-pratica.js
@@ -0,0 +1,10 @@
+const joao = 80
+if (joao >= 80) {
+    console.log("Parabéns, você foi aprovado")
+}
+else if (joao < 80 && joao >= 60) {
+    console.log("Você esta em nossa lista de espera")
+}
+else {
+    console.log("Você foi reprovado")
+}
