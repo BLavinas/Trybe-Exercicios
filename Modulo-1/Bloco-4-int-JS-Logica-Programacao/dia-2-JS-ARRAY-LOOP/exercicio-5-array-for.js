92f3f4d9aabbd45fbdc351d0999a139198ceed00
Bernardo Lavinas <bernardorclavinas@gmail.com>
2022-06-22 20:47:06 -0300

    Exercicio 5 realizado.

---
+++ Modulo-1/Bloco-4-int-JS-Logica-Programacao/dia-2-JS-ARRAY-LOOP/exercicio-5-array-for.js
@@ -0,0 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let higherNumber = numbers[0]
+
+for (index = 0; index < numbers.length; index += 1) {
+if (higherNumber < numbers[index]) {
+higherNumber = numbers[index]
+}
+}
+console.log(higherNumber);
