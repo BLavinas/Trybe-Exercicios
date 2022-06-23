fd6e4c9cf2d04c087a9699c0ddf51b88fb0f541c
Bernardo Lavinas <bernardorclavinas@gmail.com>
2022-06-23 17:08:09 -0300

    Divisao por 2 no array. Exercicio 9 realizado

---
+++ Modulo-1/Bloco-4-int-JS-Logica-Programacao/dia-2-JS-ARRAY-LOOP/exercicio-9-array-for.js
@@ -0,0 +1,11 @@
+let numbers = [];
+for (i = 1; i < 26; i += 1) {
+    numbers.push(i)
+}
+console.log(numbers);
+
+ 
+
+for (index = 0; index < numbers.length; index += 1){
+console.log(numbers[index] / 2);
+}
